33eb5b86eead6e329b48d15081985f8f64ba20b3
KonstMSU <KonstMSU@localhost>
2009-07-18 18:09:25 +0000

    Added a couple of classes to Shared

---
+++ Magenta.Shared/Exceptions/ExceptionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magenta.Shared.Exceptions
+{
+    public static class ExceptionFactory
+    {
+        public static ArgumentNullException ArgumentNull(Exception innerException, string format, params object[] args)
+        {
+            return new ArgumentNullException(string.Format(format, args), innerException);
+        }
+
+        public static ArgumentNullException ArgumentNull(string format, params object[] args)
+        {
+            return ArgumentNull(null, format, args);
+        }
+    }
+}
