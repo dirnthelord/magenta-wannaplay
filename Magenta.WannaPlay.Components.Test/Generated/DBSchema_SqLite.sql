368a22dd6dbf45ad324014d348efbb57fb9ef954
KonstMSU <KonstMSU@localhost>
2009-07-18 16:23:59 +0000

    Fixed mapping generation

--- Magenta.WannaPlay.Components.Test/Generated/DBSchema_SqLite.sql
+++ Magenta.WannaPlay.Components.Test/Generated/DBSchema_SqLite.sql
@@ -1,37 +1,10 @@
-drop table if exists "Entity"
-drop table if exists DutyGuard
-drop table if exists BookingSlot
-drop table if exists Resident
-drop table if exists Facility
-drop table if exists ResidenceAddress
-create table "Entity" (Id  integer, primary key (Id))
-create table DutyGuard (
-  EntityId INTEGER not null,
-   primary key (EntityId)
-)
-create table BookingSlot (
-  EntityId INTEGER not null,
-   From DATETIME,
-   To DATETIME,
-   Facility_id INTEGER,
-   Resident_id INTEGER,
-   BookedBy_id INTEGER,
-   primary key (EntityId)
-)
-create table Resident (
-  EntityId INTEGER not null,
-   Number INTEGER,
-   Address_id INTEGER,
-   primary key (EntityId)
-)
-create table Facility (
-  EntityId INTEGER not null,
-   Name TEXT,
-   primary key (EntityId)
-)
-create table ResidenceAddress (
-  EntityId INTEGER not null,
-   Block TEXT,
-   Unit TEXT,
-   primary key (EntityId)
-)
+drop table if exists "BookingSlot"
+drop table if exists "DutyGuard"
+drop table if exists "Resident"
+drop table if exists "ResidenceAddress"
+drop table if exists "Facility"
+create table "BookingSlot" (Id  integer, From DATETIME, To DATETIME, Facility_id INTEGER, Resident_id INTEGER, BookedBy_id INTEGER, primary key (Id))
+create table "DutyGuard" (Id  integer, NameFirst TEXT, NameLast TEXT, primary key (Id))
+create table "Resident" (Id  integer, NameFirst TEXT, NameLast TEXT, Number INTEGER, Address_id INTEGER, primary key (Id))
+create table "ResidenceAddress" (Id  integer, Block TEXT, Unit TEXT, primary key (Id))
+create table "Facility" (Id  integer, Name TEXT, primary key (Id))
--- Magenta.WannaPlay.Components/Domain/WannaPlayPersistenceModel.cs
+++ Magenta.WannaPlay.Components/Domain/WannaPlayPersistenceModel.cs
@@ -4,17 +4,37 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using FluentNHibernate.AutoMap;
+using FluentNHibernate.MappingModel;
+using FluentNHibernate.MappingModel.Identity;
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Mapping;
 
 namespace Magenta.WannaPlay.Domain
 {
+    //class PrimaryKeyConvention :  IIdConvention  
+    //{
+    //    public bool Accept(IIdentityPart target)
+    //    {
+    //        return true;
+    //    }
+
+    //    public void Apply(IIdentityPart target)
+    //    {
+    //        target.GeneratedBy.Sequence(target.Property.ReflectedType + "_seq");
+    //    }
+    //}
+
     public class WannaPlayPersistenceModel : AutoPersistenceModel
     {
         public WannaPlayPersistenceModel()
         {
             AddEntityAssembly(Assembly.GetExecutingAssembly())
-                .Where(entity => entity.BaseType == typeof(Entity) && entity.IsAbstract == false);
+                .Where(entity => entity.BaseType == typeof(Entity)/* && entity.IsAbstract == false*/);
+
+            //ConventionDiscovery.Add<PrimaryKeyConvention>();
 
             WithSetup(s => s.IsComponentType = type => type == typeof(PersonName));
+            WithSetup(s => s.IsBaseType = type => type == typeof(Entity));
         }
     }
 }
