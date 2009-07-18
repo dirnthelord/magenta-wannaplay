96c3aadf5e30cf857d9ac1dbab6dcb2eaec1698c
KonstMSU <KonstMSU@localhost>
2009-07-18 16:25:48 +0000

    Fixed mapping generation

--- Magenta.WannaPlay.Components/Domain/WannaPlayPersistenceModel.cs
+++ Magenta.WannaPlay.Components/Domain/WannaPlayPersistenceModel.cs
@@ -11,27 +11,12 @@ using FluentNHibernate.Mapping;
 
 namespace Magenta.WannaPlay.Domain
 {
-    //class PrimaryKeyConvention :  IIdConvention  
-    //{
-    //    public bool Accept(IIdentityPart target)
-    //    {
-    //        return true;
-    //    }
-
-    //    public void Apply(IIdentityPart target)
-    //    {
-    //        target.GeneratedBy.Sequence(target.Property.ReflectedType + "_seq");
-    //    }
-    //}
-
     public class WannaPlayPersistenceModel : AutoPersistenceModel
     {
         public WannaPlayPersistenceModel()
         {
             AddEntityAssembly(Assembly.GetExecutingAssembly())
-                .Where(entity => entity.BaseType == typeof(Entity)/* && entity.IsAbstract == false*/);
-
-            //ConventionDiscovery.Add<PrimaryKeyConvention>();
+                .Where(entity => entity.BaseType == typeof(Entity));
 
             WithSetup(s => s.IsComponentType = type => type == typeof(PersonName));
             WithSetup(s => s.IsBaseType = type => type == typeof(Entity));
