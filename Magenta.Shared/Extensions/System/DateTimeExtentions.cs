5f3c14e7dce771d1ba1b3705ded378ad184fa891
andrey.tsykunov <andrey.tsykunov@localhost>
2009-07-19 04:42:33 +0000

    Added Shared.Test Fixed BookingSlot mapping test

---
+++ Magenta.Shared/Extensions/System/DateTimeExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class DateTimeExtentions
+    {
+        public static DateTime RoundToSeconds(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day,
+                dateTime.Hour, dateTime.Minute, dateTime.Second, dateTime.Kind);
+        }
+    }
+}
---
+++ Magents.Shared.Test/Extentions/System/DateTimeExtentionsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace System
+{
+    [TestFixture]
+    public class DateTimeExtentionsTest
+    {
+        [Test]
+        public void RoundToSeconds()
+        {
+            while(true)
+            {
+                DateTime now = DateTime.UtcNow;
+
+                if(now.Millisecond > 0)
+                {
+                    now.RoundToSeconds().Millisecond.AssertIsEqual(0);
+                    break;
+                }
+            }
+        }
+    }
+}
