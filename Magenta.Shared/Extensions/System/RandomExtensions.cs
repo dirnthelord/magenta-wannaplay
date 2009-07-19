20c8d7d8f8ec3de0cada17ae91d2408e8787ca08
KonstMSU <KonstMSU@localhost>
2009-07-19 02:46:30 +0000

    Moved RandomExtensions to System namespace

--- Magenta.Shared/Extensions/System/RandomExtensions.cs
+++ Magenta.Shared/Extensions/System/RandomExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Magenta.Shared.Extensions.System
+namespace System
 {
     public static class RandomExtensions
     {
