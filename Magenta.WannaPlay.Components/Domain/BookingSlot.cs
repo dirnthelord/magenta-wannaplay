51810778253a99efb217c7bb58d25c3101e5d55f
KonstMSU <KonstMSU@localhost>
2009-07-19 16:38:36 +0000

    Added Period to BookingSlot

--- Magenta.WannaPlay.Components/Domain/BookingSlot.cs
+++ Magenta.WannaPlay.Components/Domain/BookingSlot.cs
@@ -1,4 +1,5 @@
 using System;
+using Magenta.Shared;
 
 namespace Magenta.WannaPlay.Domain
 {
@@ -15,5 +16,8 @@ namespace Magenta.WannaPlay.Domain
         public virtual DutyGuard BookedByGuard { get; set; }
 
         public virtual DateTime BookedAt { get; set; }
+
+        // TODO: Make this as component in NHibernate, remove From and To
+        public virtual DateTimePeriod Period { get { return new DateTimePeriod(FromTime, ToTime); } }
     }
 }
