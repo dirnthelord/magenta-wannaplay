9da6237a9d2d64d4d422719bfe8f671376f898fe
KonstMSU <KonstMSU@localhost>
2009-07-19 17:21:18 +0000

    Added DateTimeHelper DateTimePeriod is mutable to allow mapping to database and simple serialization Added EquatableBase

---
+++ Magenta.Shared/EquatableBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magenta.Shared
+{
+    public abstract class EquatableBase<T> : IEquatable<T>
+        where T: EquatableBase<T>
+    {
+        public bool Equals(T other)
+        {
+            if (other == null)
+                return false;
+
+            return EqualsCoreNotNull(other);
+        }
+
+        protected abstract bool EqualsCoreNotNull(T other);
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is T))
+                return false;
+
+            return Equals((T)obj);
+        }
+
+        /// <summary>
+        /// Returns 0 to make sure hash codes of equal values should always be equal
+        /// Override to provide smarter behaviour
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public static bool operator ==(T left, EquatableBase<T> right)
+        {
+            return EqualityComparer<T>.Default.Equals(left, (T)right);
+        }
+
+        public static bool operator !=(T left, EquatableBase<T> right)
+        {
+            return !(left == right);
+        }
+    }
+}
