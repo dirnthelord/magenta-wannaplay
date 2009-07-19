7bdff262468ac3896f6ecdaaf237da6d15bbf3b3
KonstMSU <KonstMSU@localhost>
2009-07-19 18:59:37 +0000

    Added more assertions

--- Magenta.Shared.Testing/Assetions.cs
+++ Magenta.Shared.Testing/Assetions.cs
@@ -3,10 +3,10 @@ namespace NUnit.Framework
 {
     public static class Assetions
     {
-        public static void AssertIsEqual(this object left, object right)
-        {
-            Assert.AreEqual(left, right);
-        }
+        //public static void AssertIsEqual(this object left, object right)
+        //{
+        //    Assert.AreEqual(left, right);
+        //}
 
         public static void AssertIsEqual<T>(this T left, T right)
         {
@@ -21,6 +21,16 @@ namespace NUnit.Framework
         public static void AssertIsNotNull(this object left)
         {
             Assert.IsNotNull(left);
+        }
+
+        public static void AssertIsTrue(this bool value)
+        {
+            Assert.IsTrue(value);
+        }
+
+        public static void AssertIsFalse(this bool value)
+        {
+            Assert.IsFalse(value);
         }
     }
 }
