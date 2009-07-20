db855335901d1931834a5e82b9a85c779100213c
KonstMSU <KonstMSU@localhost>
2009-07-20 15:16:29 +0000

    Refactoring

--- Magenta.WannaPlay/Magenta.WannaPlay.UI.WinForms/Services/IGranularScheduleBookingService.cs
+++ Magenta.WannaPlay/Magenta.WannaPlay.UI.WinForms/Services/IGranularScheduleBookingService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Magenta.WannaPlay.UI.WinForms.ViewModels;
 using Magenta.Shared;
 using Magenta.WannaPlay.Domain;
+using Magenta.WannaPlay.UI.WinForms.Domain;
 
 namespace Magenta.WannaPlay.UI.WinForms.Services
 {
