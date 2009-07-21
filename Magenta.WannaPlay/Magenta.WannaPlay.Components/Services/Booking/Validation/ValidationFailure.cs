f642f451bd2f47dd64b115b89b4914df3adfeaf0
andrey.tsykunov <andrey.tsykunov@localhost>
2009-07-21 14:41:56 +0000

    added validation rules prototype

---
+++ Magenta.WannaPlay/Magenta.WannaPlay.Components/Services/Booking/Validation/ValidationFailure.cs
@@ -0,0 +1,20 @@
+namespace Magenta.WannaPlay.Services.Booking.Validation
+{
+    public class ValidationFailure
+    {
+        public ValidationFailure(string errorId)
+            : this(errorId, null)
+        {
+        }
+
+        public ValidationFailure(string errorId, object[] parameters)
+        {
+            ErrorId = errorId;
+            Parameters = parameters;
+        }
+
+        public string ErrorId { get; private set; }
+
+        public object[] Parameters { get; private set; }
+    }
+}
---
+++ Magenta.WannaPlay/Magenta.WannaPlay.Components/Services/Booking/Validation/ValidationRules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magenta.WannaPlay.Services.Booking.Validation
+{
+
+    public class ValidationRules<T>
+    {
+        List<ValidationRule<T>> _rules = new List<ValidationRule<T>>();
+
+        public IEnumerable<ValidationFailure> Validate(T target)
+        {
+            return _rules.Aggregate(
+                new List<ValidationFailure>(), (failures, rule) =>
+                {
+                   if (!rule.Validator(target))
+                       failures.Add(rule.Failure);
+                   return failures;
+                })
+                .ToList();
+        }
+
+        public void Add(ValidationRule<T> rule)
+        {
+            _rules.Add(rule);
+        }
+    }
+}
