4d2c6dae803893b2c61e54667d0c100ca1249ad6
andrey.tsykunov <andrey.tsykunov@localhost>
2009-07-21 14:41:56 +0000

    added validation rules prototype

---
+++ Magenta.WannaPlay/Magenta.WannaPlay.Components/Services/Booking/Validation/ValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magenta.WannaPlay.Services.Booking.Validation
+{
+    public class ValidationRule<T>
+    {
+        /// <summary>
+        /// True if validation rule succeded, False otherwise
+        /// </summary>
+        public Func<T, bool> Validator { get; private set; }
+
+        public ValidationFailure Failure { get; private set; }
+
+        public ValidationRule(Func<T, bool> validator, ValidationFailure failure)
+        {
+            Validator = validator;
+            Failure = failure;
+        }
+    }
+}
