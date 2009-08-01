d4d18fa857f1aa260e6573f6adc7d2546872977d
andrey.tsykunov <andrey.tsykunov@localhost>
2009-08-01 10:24:14 +0000

    Refactored error handling Added post-build step to copy sql ce libs

--- Magenta.WannaPlay/Magenta.WannaPlay.UI.WinForms/Program.cs
+++ Magenta.WannaPlay/Magenta.WannaPlay.UI.WinForms/Program.cs
@@ -12,14 +12,31 @@ namespace Magenta.WannaPlay.UI.WinForms
 {
     static class Program
     {
+        /// <summary>
+        /// LogManager should be called from main assembly
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
         [STAThread]
         static void Main()
         {
-            // TODO: Remove
-            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-SG");
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-SG");
+            Log.Info("Application has started");
 
-            new WannaPlayApplication(new ComponentsConfiguration()).Run();
+            try
+            {
+                // TODO: Remove
+                //Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-SG");
+                //Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-SG");
+
+                new WannaPlayApplication(new ComponentsConfiguration()).Run();
+
+                Log.Info("Application has stopped");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Initialization error");
+                Log.Error("Initialization exception", e);
+            }
         }
     }
 }
