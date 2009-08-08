2afc5548f3bd04197c5045120df4fecc9c7a711b
KonstMSU <KonstMSU@localhost>
2009-08-08 08:32:19 +0000

    Added class EventExtensions

---
+++ _shared/Magenta.Shared/Extensions/System/EventExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace System
+{
+    public static class EventExtensions
+    {
+        public static void Raise<TEventArgs>(this EventHandler<TEventArgs> eventHandler, object sender, TEventArgs e)
+            where TEventArgs: EventArgs
+        {
+            if (eventHandler != null)
+                eventHandler(sender, e);
+        }
+
+        public static void Raise(this EventHandler eventHandler, object sender, EventArgs e)
+        {
+            if (eventHandler != null)
+                eventHandler(sender, e);
+        }
+
+        public static void Raise(this PropertyChangedEventHandler eventHandler, object sender, string propertyName)
+        {
+            if (eventHandler != null)
+                eventHandler(sender, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
