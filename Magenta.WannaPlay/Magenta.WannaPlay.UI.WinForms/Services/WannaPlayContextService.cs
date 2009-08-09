e2e279168b382c69c010d6badab75a1d6e1d6d6b
andrey.tsykunov <andrey.tsykunov@localhost>
2009-08-09 04:02:22 +0000

    Saving last used DutyGuard

--- Magenta.WannaPlay/Magenta.WannaPlay.UI.WinForms/Services/WannaPlayContextService.cs
+++ Magenta.WannaPlay/Magenta.WannaPlay.UI.WinForms/Services/WannaPlayContextService.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Magenta.WannaPlay.Domain;
+using Magenta.WannaPlay.Services.Residence;
 
 namespace Magenta.WannaPlay.UI.WinForms.Services
 {
     public class WannaPlayContextService : IWannaPlayContextService
     {
+        private readonly IResidenceManager _residenceManager;
+
         #region CurrentGuard
         DutyGuard _currentGuard;
 
         public DutyGuard CurrentGuard
         {
             get { return _currentGuard; }
-            set { _currentGuard = value; OnCurrentGuardChanged(); }
+            set
+            {
+                _currentGuard = value;
+                _currentGuard.LoggedLastTime = DateTime.UtcNow;
+                _residenceManager.SaveDutyGuard(_currentGuard);
+
+                OnCurrentGuardChanged();
+            }
         }
 
         public event Action CurrentGuardChanged;
@@ -47,9 +57,15 @@ namespace Magenta.WannaPlay.UI.WinForms.Services
         }
         #endregion
 
-        public WannaPlayContextService()
+        public WannaPlayContextService(IResidenceManager residenceManager)
         {
+            _residenceManager = residenceManager;
+
             SelectedDay = DateTime.Today;
+
+            var guards = residenceManager.GetDutyGuards();
+            if (guards.Count() > 0)
+                CurrentGuard = guards.OrderByDescending(x => x.LoggedLastTime).First();
         }
     }
 }
