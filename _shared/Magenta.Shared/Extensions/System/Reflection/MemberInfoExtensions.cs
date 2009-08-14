25854ef8172c6fe0014c6583bb0ccf3a0ac4835e
KonstMSU <KonstMSU@localhost>
2009-08-14 15:37:52 +0000

    Added ObjectFactory and AOP-supporting classes

---
+++ _shared/Magenta.Shared/Extensions/System/Reflection/MemberInfoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace System.Reflection
+{
+    public static class MemberInfoExtensions
+    {
+        public static IEnumerable<T> GetCustomAttributes<T>(this MemberInfo member, bool inherit)
+        {
+            return member.GetCustomAttributes(typeof(T), inherit).Cast<T>();
+        }
+    }
+}
---
+++ _shared/Magents.Shared.Test/Extentions/System/ObjectExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Magenta.Shared.Extentions.System
+{
+    [TestFixture]
+    public class ObjectExtensionsTest
+    {
+        [Test]
+        public void ValueOrDefault_not_null_test()
+        {
+            var obj = new List<int> { 2 };
+            obj.ValueOrDefault(o => o.Count).AssertIsEqual(1);
+        }
+
+        [Test]
+        public void ValueOrDefault_null_test()
+        {
+            List<int> obj = null;
+            obj.ValueOrDefault(o => o.Count).AssertIsEqual(0);
+            obj.ValueOrDefault(o => o.Count, 3).AssertIsEqual(3);
+        }
+    }
+}
